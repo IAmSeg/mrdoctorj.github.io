e70a9708726c5d1eebbbaa829d3b610f6e27e154
Will Johnson <willalanjohnson@gmail.com>
2016-01-30 12:50:19 -0600

    Added contact component.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,9 +6,9 @@ new Vue({
   el: '#header',
   data: {
     links: [
-      {url: '#about', title: 'About'},
-      {url: '#projects', title: 'Projects'},
-      {url: '#contact', title: 'Contact'}
+      { url: '#about', title: 'About' },
+      { url: '#projects', title: 'Projects' },
+      { url: '#contact', title: 'Contact' }
     ]
   }
 });
@@ -18,12 +18,12 @@ new Vue({
   el: '#links',
   data: {
     links: [
-      {url: 'https://twitter.com/WillAJohnson', logo: '/images/twitter.svg#twitter',
-      viewbox: '0 0 246 209', title: 'twitter'},
-      {url: 'https://github.com/MrDoctorJ', viewbox: '0 0 256 250',
-      logo: '/images/github.svg#github', title: 'github'},
-      {url: 'http://codepen.io/willalanjohnson/', viewbox: '0 0 256 256',
-      logo: '/images/codepen.svg#codepen', title: 'codepen'}
+      { url: 'https://twitter.com/WillAJohnson', logo: '/images/twitter.svg#twitter',
+        viewbox: '0 0 246 209', title: 'twitter' },
+      { url: 'https://github.com/MrDoctorJ', viewbox: '0 0 256 250',
+        logo: '/images/github.svg#github', title: 'github' },
+      { url: 'http://codepen.io/willalanjohnson/', viewbox: '0 0 256 256',
+        logo: '/images/codepen.svg#codepen', title: 'codepen' }
     ]
   }
 });
@@ -32,7 +32,7 @@ new Vue({
 new Vue({
   el: '#about',
   data: {
-    message: 'About Me',
+    header: 'About Me',
     aboutText: `I'm a Web Developer from a small town in Missouri.
       I drink lots of coffee, write lots of code and watch lots of football.`,
     aboutText2: `If you'd like to get in touch, or see what I'm up to,
@@ -44,6 +44,30 @@ new Vue({
 new Vue({
   el: '#projects',
   data: {
-    message: 'Projects'
+    header: 'Projects',
+    projects: [
+      { url: 'https://github.com/MrDoctorJ/generator-future-webapp', title: 'generator-future-webapp',
+        desc: 'A Yeoman generator to scaffold a front-end project using gulp, babel and cssnext.' },
+      { url: 'https://github.com/MrDoctorJ/mrdoctorj.github.io', title: 'This website!',
+        desc: 'The code for this website is on GitHub! Feel free to take a look!' }
+    ]
+  }
+});
+
+// @contact
+new Vue({
+  el: '#contact',
+  data: {
+    header: 'Contact',
+    message: `Need a website build? Need help with a project?<br><br> I have experience with full stack web development
+      and I embrace learning new technologies.<br><br> You can get in touch with me through the following methods.`,
+    svgs: [
+      { url: 'https://twitter.com/WillAJohnson', logo: '/images/twitter.svg#twitter',
+        viewbox: '0 0 246 209', title: 'twitter' },
+      { url: 'https://github.com/MrDoctorJ', viewbox: '0 0 256 250',
+        logo: '/images/github.svg#github', title: 'github' },
+      { url: 'http://codepen.io/willalanjohnson/', viewbox: '0 0 256 256',
+        logo: '/images/codepen.svg#codepen', title: 'codepen' }
+    ]
   }
 });
