05454ced43a939bd90cbd0c4bb71a5df4ea14c48
Will Johnson <willalanjohnson@gmail.com>
2016-01-30 22:22:20 -0600

    Added smooth scroll.

--- app/scripts/smooth-scroll.js
+++ app/scripts/smooth-scroll.js
@@ -1,6 +1,6 @@
 let smoothScroll = Vue.extend({
   props: ['link'],
-  template: '<a data-scroll href="{{link}}"><div class="arrow"></div></a>'
+  template: '<a class="arrow-link" data-scroll href="{{link}}"><div class="arrow"></div></a>'
 });
 
 Vue.component('smooth-scroll', smoothScroll);
