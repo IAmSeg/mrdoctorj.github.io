f911b98f3ab020f09249028e98602d745a649ad2
Will Johnson <willalanjohnson@gmail.com>
2016-01-31 08:59:55 -0600

    Renamed smooth-scroll to arrow.

--- app/scripts/arrow.js
+++ app/scripts/arrow.js
@@ -1,9 +1,9 @@
-let smoothScroll = Vue.extend({
+let arrow = Vue.extend({
   props: ['link'],
   template: '<div class="arrow-parent"><a class="arrow-link" data-scroll href="{{link}}"><div class="arrow"></div></a></div>'
 });
 
-Vue.component('smooth-scroll', smoothScroll);
+Vue.component('arrow-scroll', arrow);
 
 new Vue({
   el: '.hero'
