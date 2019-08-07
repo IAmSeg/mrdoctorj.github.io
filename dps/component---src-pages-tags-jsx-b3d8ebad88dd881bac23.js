bf318d7a549e1c56bf4aa36c7d0662a33ae1bfd1
Will Johnson <willalanjohnson@gmail.com>
2019-08-07 08:48:44 -0500

    added dps site

---
+++ dps/component---src-pages-tags-jsx-b3d8ebad88dd881bac23.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{155:function(t,a,e){"use strict";e.r(a);var n=e(7),r=(e(0),e(1)),s=e.n(r),p=e(164),c=e(165),o=function(t){var a=t.pageContext.tags;return Object(n.d)(p.c,null,Object(n.d)(c.a,{title:"Tags Page"},"Gatsby Tutorial Starter"),Object(n.d)(p.a,null,Object(n.d)(c.f,{list:a})))};a.default=o,o.propTypes={pageContext:s.a.shape({tags:s.a.array})}}}]);
+//# sourceMappingURL=component---src-pages-tags-jsx-b3d8ebad88dd881bac23.js.map
