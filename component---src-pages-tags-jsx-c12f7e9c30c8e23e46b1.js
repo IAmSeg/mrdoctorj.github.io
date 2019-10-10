c3aa6645d14866ef7b2f35c339955f7f2ede6540
Will Johnson <willalanjohnson@gmail.com>
2019-10-10 10:49:13 -0500

    added more pinnacles

---
+++ component---src-pages-tags-jsx-c12f7e9c30c8e23e46b1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{238:function(t,a,e){"use strict";e.r(a);var n=e(0),c=(e(1),e(76)),r=e(122);a.default=function(t){var a=t.pageContext.tags;return Object(n.d)(c.c,null,Object(n.d)(r.a,{title:"Tags Page"},"Gatsby Tutorial Starter"),Object(n.d)(c.a,null,Object(n.d)(r.f,{list:a})))}}}]);
+//# sourceMappingURL=component---src-pages-tags-jsx-c12f7e9c30c8e23e46b1.js.map
